97150cd24327f2d5b498c3cd06f1e56ad9cbd723
eppakayalanaresh <eppakayalanaresh08@gmail.com>
2023-05-26 18:28:07 +0000

    add useeffect

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Scoreboard from './components/Scoreboard'
+
+import {GlobalStyle} from './styledComponents'
+// import Welcome from './components/Welcome'
+
+const App = () => (
+  <>
+    <GlobalStyle />
+    {/* <Welcome /> */}
+    <Scoreboard />
+  </>
+)
+
+export default App
---
+++ src/components/Welcome/index.js
@@ -0,0 +1,9 @@
+// import {useState} from 'react'
+
+// const Welcome = () => {
+//   if (true) {
+//     const [count, setCount] = useState(0)
+//   }
+//   return <h1>Welcome</h1>
+// }
+// export default Welcome
